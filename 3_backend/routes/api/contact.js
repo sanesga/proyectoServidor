7140028ff3408998044d69a88184c8ea67cb7afc
sandra <sanesga@gmail.com>
2019-09-30 00:00:18 +0200

    Testing con Jasmine funcionando

--- 3_backend/routes/api/contact.js
+++ 3_backend/routes/api/contact.js
@@ -1,12 +1,22 @@
-var router = require('express').Router();
-var email = require('../../utils/email.js');
+var router = require("express").Router();
+var email = require("../../utils/email.js");
 
-router.post('/', function(req, res, next){
- 
-  console.log("estamos en el backend recibiendo datos"+req.body);
+router.post("/", function(req, res, next) {
+  console.log("estamos en el backend recibiendo datos" + req.body);
 
-  email.sendMail(req,res);
- 
+  console.log("nombre" + req.body.name);
+
+  email.sendMail(req, res);
 });
 
-module.exports = router;
+function comprobarDatos(datos) {
+  
+  if(datos!=""){
+    return true;
+   }else{
+   return false;
+ }
+}
+
+module.exports.router = router;
+module.exports.comprobarDatos = comprobarDatos;
